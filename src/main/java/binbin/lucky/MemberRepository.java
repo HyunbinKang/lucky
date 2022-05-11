5af85d0521a8cd3c865c43486b38877d118a62c7
HyunbinKang <nhd2525@gmail.com>
2022-05-11 22:37:38 +0900

    멤버 작성

---
+++ src/main/java/binbin/lucky/MemberRepository.java
@@ -0,0 +1,25 @@
+package binbin.lucky;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    // 저장하는 코드
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+    // 조회
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+
+}
